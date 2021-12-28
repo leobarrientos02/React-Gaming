c4510248e435c1cf13ae9a82713fd8d4bb9b7f35
Your Name <leobarrientos02@gmail.com>
2021-12-28 16:11:58 -0500

    more reducers

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {Provider} from 'react-redux';
+import gamesReducer from './reducers/gamesReducers';
+
 //REDUX SETUP
 import {createStore} from 'redux';
+import rootReducer from './reducers';
 
-const store = createStore();
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/reducers/gamesReducers.js
+++ src/reducers/gamesReducers.js
@@ -1,17 +1,18 @@
 const initState = {
-    popular = [],
-    newGames = [],
-    upComing = []
-}
+  popular: [],
+  newGames: [],
+  upComing: [],
+  searched: [],
+};
 
 // When we dispatch an action called "FETCH_GAMES" then it return to the state
-const gamesReducer = (state=initState,action) => {
-    switch(action.type){
-        case 'FETCH_GAMES':
-            return {...state}
-        default:
-            return {...state}   
-    }
-}
+const gamesReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "FETCH_GAMES":
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
 
 export default gamesReducer;
