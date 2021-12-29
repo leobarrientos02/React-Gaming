fa3811dc98c499469c9c8ac413a8956575f0798d
Your Name <leobarrientos02@gmail.com>
2021-12-29 15:23:11 -0500

    first page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+// REDUX
+import { useDispatch } from "react-redux";
+import { loadGames } from "../actions/gameAction";
+
+const Home = () => {
+    return(
+        <div>
+            <h2>Leo</h2>
+        </div>
+    );
+};
+
+export default Home;
