133193fd772831143e2c92102c64d09faff99d95
Your Name <leobarrientos02@gmail.com>
2021-12-29 14:55:28 -0500

    api call with axios

--- src/api.js
+++ src/api.js
@@ -37,7 +37,7 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Popular Games
-const popular_games = `games?key=${process.env.REACT_APP_RAWG_API}dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const popular_games = `games?key=${process.env.REACT_APP_RAWG_API}/dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 
