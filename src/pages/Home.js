31d75aae83d8ae26ab317423ca815923e7d8f130
Your Name <leobarrientos02@gmail.com>
2021-12-30 21:40:39 -0500

    cleaning up the home page a little

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React,{useEffect} from "react";
 // REDUX
 import { useDispatch } from "react-redux";
 import { loadGames } from "../actions/gameAction";
 
 const Home = () => {
+    const dispatch = useDispatch();
+    useEffect(() =>{
+      dispatch(loadGames());
+      //console.log(loadGames());
+    })
     return(
         <div>
             <h2>Leo</h2>
