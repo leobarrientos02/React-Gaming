8f05d6b599f9eaa16735567087624abed59274bb
Your Name <leobarrientos02@gmail.com>
2022-03-30 19:05:28 -0400

    Bug fixes

--- src/api.js
+++ src/api.js
@@ -26,6 +26,8 @@ const getCurrentDay = () => {
   }
 };
 
+const HHH = "daead6f258e24d6791a190d12e89f7e0";
+
 // Current day/month/year
 const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
@@ -36,13 +38,13 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Popular Games
-const popular_games = `games?key=${process.env.REACT_APP_RAWG_API}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const popular_games = `games?key=${HHH}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 
 // Upcoming games
-const upComing_games = `games?key=${process.env.REACT_APP_RAWG_API}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const upComing_games = `games?key=${HHH}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
 
 // New Games
-const new_games = `games?key=${process.env.REACT_APP_RAWG_API}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const new_games = `games?key=${HHH}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 
@@ -52,11 +54,12 @@ export const newGamesURL = () => `${base_url}${new_games}`;
 
 // GAME DETAILS
 export const gameDetailsURL = (game_id) =>
-  `${base_url}games/${game_id}?&key=${process.env.REACT_APP_RAWG_API}`;
+  `${base_url}games/${game_id}?&key=${HHH}`;
 
 // GAME SCREENSHOTS
 export const gameScreenshotURL = (game_id) =>
-  `${base_url}games/${game_id}/screenshots?&key=${process.env.REACT_APP_RAWG_API}`;
+  `${base_url}games/${game_id}/screenshots?&key=${HHH}`;
 
 //  SEARCHED GAMES
-export const searchGameURL = (game_name) => `${base_url}games?key=${process.env.REACT_APP_RAWG_API}&search=${game_name}&page_size=9`;
+export const searchGameURL = (game_name) =>
+  `${base_url}games?key=${HHH}&search=${game_name}&page_size=9`;
--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -1,39 +1,38 @@
-import React from 'react';
+import React from "react";
 // STYLING AND ANIMATION
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
+import styled from "styled-components";
+import { motion } from "framer-motion";
 // REDUx
-import {useSelector} from 'react-redux';
-import {useNavigate} from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 // IMAGE SIZE REDUCER
-import { smallImage } from '../util';
+import { smallImage } from "../util";
 
 // IMPORTING IMAGES
-import playstation from '../img/playstation.svg';
-import steam from '../img/steam.svg';
-import xbox from '../img/xbox.svg';
-import nintendo from '../img/nintendo.svg';
-import apple from '../img/apple.svg';
-import gamepad from '../img/gamepad.svg';
+import playstation from "../img/playstation.svg";
+import steam from "../img/steam.svg";
+import xbox from "../img/xbox.svg";
+import nintendo from "../img/nintendo.svg";
+import apple from "../img/apple.svg";
+import gamepad from "../img/gamepad.svg";
 
 //Star Images
 import starEmpty from "../img/star-empty.png";
 import starFull from "../img/star-full.png";
 
+const GameDetail = ({ pathId }) => {
+  const navigate = useNavigate();
+  //Exit Detail
+  const exitDetailHander = (e) => {
+    const element = e.target;
+    if (element.classList.contains("shadow")) {
+      document.body.style.overflow = "auto";
+      navigate("/");
+    }
+  };
 
-const GameDetail = ({ pathId }) =>{
-    const navigate = useNavigate();
-    //Exit Detail
-    const exitDetailHander = (e) => {
-        const element = e.target;
-        if (element.classList.contains("shadow")) {
-            document.body.style.overflow = "auto";
-            navigate('/');
-        }
-    };
-
-    //Get Stars
-    const getStars = () => {
+  //Get Stars
+  const getStars = () => {
     const stars = [];
     const rating = Math.floor(game.rating);
     for (let i = 1; i <= 5; i++) {
@@ -43,84 +42,99 @@ const GameDetail = ({ pathId }) =>{
         stars.push(<img alt="star" key={i} src={starEmpty}></img>);
       }
     }
-      return stars;
-    };
-    
-    // GET PLATFORM IMAGES
-    const getPlatform = (platform) =>{
-        if (platform.includes("PlayStation")) {
-            return playstation;
-          } else if (platform.includes("Xbox")) {
-            return xbox;
-          } else if (platform === "PC") {
-            return steam;
-          } else if (platform === "Nintendo Switch") {
-            return nintendo;
-          } else if (platform.includes("OS")) {
-            return apple;
-          } else {
-            return gamepad;
-          }
+    return stars;
+  };
+
+  // GET PLATFORM IMAGES
+  const getPlatform = (platform) => {
+    if (platform.includes("PlayStation")) {
+      return playstation;
+    } else if (platform.includes("Xbox")) {
+      return xbox;
+    } else if (platform === "PC") {
+      return steam;
+    } else if (platform === "Nintendo Switch") {
+      return nintendo;
+    } else if (platform.includes("OS")) {
+      return apple;
+    } else {
+      return gamepad;
     }
-    // GET PLATFORM URL
-    const getPlatformURL = (platform) =>{
-        if (platform.includes("PlayStation")) {
-            return "https://www.playstation.com/en-us/";
-          } else if (platform.includes("Xbox")) {
-            return "https://www.xbox.com/en-US";
-          } else if (platform === "PC") {
-            return "https://store.steampowered.com/";
-          } else if (platform === "Nintendo Switch") {
-            return "https://www.nintendo.com/";
-          } else if (platform.includes("OS")) {
-            return "https://www.apple.com/";
-          } else {
-            return "https://www.omfgdogs.com/#";
-          }
-    }    
+  };
+  // GET PLATFORM URL
+  const getPlatformURL = (platform) => {
+    if (platform.includes("PlayStation")) {
+      return "https://www.playstation.com/en-us/";
+    } else if (platform.includes("Xbox")) {
+      return "https://www.xbox.com/en-US";
+    } else if (platform === "PC") {
+      return "https://store.steampowered.com/";
+    } else if (platform === "Nintendo Switch") {
+      return "https://www.nintendo.com/";
+    } else if (platform.includes("OS")) {
+      return "https://www.apple.com/";
+    } else {
+      return "https://www.omfgdogs.com/#";
+    }
+  };
 
-
-    // DATA
-    const  {screen, game, isLoading } = useSelector((state) => state.detail);
-    return(
-        <>
-        {!isLoading && (
-        <CardShadow 
-            className="shadow" 
-            onClick={exitDetailHander}
-        >
-            <Detail layoutId={pathId}>
-                <Stats>
-                    <div className="rating">
-                        <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
-                        <p>Rating: {game.rating}</p>
-                        {getStars()}
-                    </div>
-                    <Info>
-                        <h3>Platforms</h3>
-                        <Platforms>
-                            {game.platforms.map((data) => (
-                                <a href={getPlatformURL(data.platform.name)} target="_blank" rel="noreferrer">
-                                    <img key={data.platform.id} src={getPlatform(data.platform.name)} alt={data.platform.name} title={data.platform.name} />
-                                </a>
-                            ))}
-                        </Platforms>
-                    </Info>
-                </Stats>
-                <Media>
-                    <motion.img layoutId={`image ${pathId}`} src={smallImage(game.background_image, 1280)} alt={game.background_image} />
-                </Media>
-                <Description>
-                    <motion.p layoutId={`description ${pathId}`}>{game.description_raw}</motion.p>
-                </Description>
-                <h3>Screenshots</h3>
-                <div className="gallery">
-                    {screen.results.map((image) => (
-                        <img src={smallImage(image.image, 1280)} key={image.id} alt={screen.image} />
-                    ))}
-                </div>
-                <Info2>
-                    {/* <h4>ESRB Rating</h4>
+  // DATA
+  const { screen, game, isLoading } = useSelector((state) => state.detail);
+  return (
+    <>
+      {!isLoading && (
+        <CardShadow className="shadow" onClick={exitDetailHander}>
+          <Detail layoutId={pathId}>
+            <Stats>
+              <div className="rating">
+                <motion.h3 layoutId={`title ${pathId}`}>{game.name}</motion.h3>
+                <p>Rating: {game.rating}</p>
+                {getStars()}
+              </div>
+              <Info>
+                <h3>Platforms</h3>
+                <Platforms>
+                  {game.platforms.map((data) => (
+                    <a
+                      href={getPlatformURL(data.platform.name)}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <img
+                        key={data.platform.id}
+                        src={getPlatform(data.platform.name)}
+                        alt={data.platform.name}
+                        title={data.platform.name}
+                      />
+                    </a>
+                  ))}
+                </Platforms>
+              </Info>
+            </Stats>
+            <Media>
+              <motion.img
+                layoutId={`image ${pathId}`}
+                src={smallImage(game.background_image, 1280)}
+                alt={game.background_image}
+              />
+            </Media>
+            <Description>
+              <motion.p layoutId={`description ${pathId}`}>
+                {game.description_raw}
+              </motion.p>
+            </Description>
+            <h3>Screenshots</h3>
+            <div className="gallery">
+              {screen.results.map((image) => (
+                <img
+                  src={smallImage(image.image, 1280)}
+                  key={image.id}
+                  alt={screen.image}
+                />
+              ))}
+            </div>
+            <Info2>
+              {/* <h4>ESRB Rating</h4>
                     <div className="esrb">
                             if(game.esrb_rating.name == "null"){
                                 <p>None</p>
@@ -128,57 +142,45 @@ const GameDetail = ({ pathId }) =>{
                                 <p>{game.esrb_rating.name}</p>
                             }
                     </div> */}
-                    <h4>Genres</h4>
-                    <div className="genres">
-                        {game.genres.map((genre) => (
-                            <div className="genre" key={genre.id}>
-                                <p>
-                                    {genre.name}
-                                </p>
-                            </div>
-                        ))}
-                    </div>
-                    <h4>Developers</h4>
-                    <div className="developers">
-                        {game.developers.map((developer) => (
-                            <div className="developer" key={developer.id}>
-                                <p>
-                                    {developer.name}
-                                </p>
-                            </div>
-                        ))}
-                    </div>
-                    <h4>Publishers</h4>
-                    <div className="publishers">
-                        {game.publishers.map((publisher) => (
-                            <div className="publisher" key={publisher.id}>
-                                <p>
-                                    {publisher.name}
-                                </p>
-                            </div>
-                        ))}
-                    </div>
-                    <h4>Tags</h4>
-                    <Tags>
-                        {game.tags.map((tag) => (
-                            <Tag key={tag.id}>
-                                <p>
-                                    {tag.name}
-                                </p>
-                            </Tag>
-    
-                        ))}
-                    </Tags>                    
-                    
-                </Info2>
-
-            </Detail>
+              <h4>Genres</h4>
+              <div className="genres">
+                {game.genres.map((genre) => (
+                  <div className="genre" key={genre.id}>
+                    <p>{genre.name}</p>
+                  </div>
+                ))}
+              </div>
+              <h4>Developers</h4>
+              <div className="developers">
+                {game.developers.map((developer) => (
+                  <div className="developer" key={developer.id}>
+                    <p>{developer.name}</p>
+                  </div>
+                ))}
+              </div>
+              <h4>Publishers</h4>
+              <div className="publishers">
+                {game.publishers.map((publisher) => (
+                  <div className="publisher" key={publisher.id}>
+                    <p>{publisher.name}</p>
+                  </div>
+                ))}
+              </div>
+              <h4>Tags</h4>
+              <Tags>
+                {game.tags.map((tag) => (
+                  <Tag key={tag.id}>
+                    <p>{tag.name}</p>
+                  </Tag>
+                ))}
+              </Tags>
+            </Info2>
+          </Detail>
         </CardShadow>
-        )}
-        </>
-    );
+      )}
+    </>
+  );
 };
-
 
 const CardShadow = styled(motion.div)`
   width: 100%;
@@ -249,33 +251,28 @@ const Description = styled(motion.div)`
   margin: 5rem 0rem;
 `;
 
-
 const Info2 = styled(motion.div)`
-    h4{
-        margin: 1rem 0rem;
-    }
+  h4 {
+    margin: 1rem 0rem;
+  }
 `;
 const Tags = styled(motion.div)`
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    flex-wrap: wrap;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
 `;
 
 const Tag = styled(motion.div)`
-    p{
-        font-size: 1rem;
-        padding: .25rem .5rem;
-        background: #696969;
-        color: white;
-        border-radius: 10px;
-        margin: .5rem .75rem;
-    }
-`;
-
-const Stars = styled(motion.div)`
-    font-size: 2rem;
+  p {
+    font-size: 1rem;
+    padding: 0.25rem 0.5rem;
+    background: #696969;
+    color: white;
+    border-radius: 10px;
+    margin: 0.5rem 0.75rem;
+  }
 `;
 
 export default GameDetail;
