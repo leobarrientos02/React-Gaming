d17f720b7e6ddd34db35d80ed1e3722dab02db8f
Your Name <leobarrientos02@gmail.com>
2022-04-01 12:03:36 -0400

    bug edits

--- src/api.js
+++ src/api.js
@@ -26,8 +26,6 @@ const getCurrentDay = () => {
   }
 };
 
-const HHH = "daead6f258e24d6791a190d12e89f7e0";
-
 // Current day/month/year
 const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
@@ -38,13 +36,13 @@ const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Popular Games
-const popular_games = `games?key=${HHH}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const popular_games = `games?key=${process.env.REACT_APP_API_KEY}&dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 
 // Upcoming games
-const upComing_games = `games?key=${HHH}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const upComing_games = `games?key=${process.env.REACT_APP_API_KEY}&dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
 
 // New Games
-const new_games = `games?key=${HHH}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
+const new_games = `games?key=${process.env.REACT_APP_API_KEY}&dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = () => `${base_url}${popular_games}`;
 
@@ -54,12 +52,12 @@ export const newGamesURL = () => `${base_url}${new_games}`;
 
 // GAME DETAILS
 export const gameDetailsURL = (game_id) =>
-  `${base_url}games/${game_id}?&key=${HHH}`;
+  `${base_url}games/${game_id}?&key=${process.env.REACT_APP_API_KEY}`;
 
 // GAME SCREENSHOTS
 export const gameScreenshotURL = (game_id) =>
-  `${base_url}games/${game_id}/screenshots?&key=${HHH}`;
+  `${base_url}games/${game_id}/screenshots?&key=${process.env.REACT_APP_API_KEY}`;
 
 //  SEARCHED GAMES
 export const searchGameURL = (game_name) =>
-  `${base_url}games?key=${HHH}&search=${game_name}&page_size=9`;
+  `${base_url}games?key=${process.env.REACT_APP_API_KEY}&search=${game_name}&page_size=9`;
